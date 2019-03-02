ef758643c474afc99ebe3ac9862cb4ca50e45812
Vamsi Pudoor <vipudoor@Vamsis-MacBook-Pro-2.local>
2019-03-02 14:49:20 +0530

    switching to develop from master

--- src/diet/Diet.js
+++ src/diet/Diet.js
@@ -1,29 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
   View,
   ImageBackground,
-  Dimensions,
-} from 'react-native';
-import { Input, Button } from 'react-native-elements';
-import DietGoalPlan from './DietGoalPlan';
+  Dimensions
+} from "react-native";
+import { Input, Button } from "react-native-elements";
+import DietGoalPlan from "./DietGoalPlan";
+import SelectFoodSources from "./SelectFoodSources";
 
 export default class Diet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectionLevel: 1,
+      totalLevels: 2,
+      selectedProteinSources: [],
+      selectedFatSources: [],
+      selectedCarbSources: []
+    };
+  }
+
+  changeSelectionLevel = progress => {
+    const { selectionLevel, totalLevels } = this.state;
+    const nextLevel = progress ? selectionLevel + 1 : selectionLevel - 1;
+    if (nextLevel > 0 && nextLevel <= totalLevels) {
+      this.setState({ selectionLevel: nextLevel });
+    }
+  };
+
   render() {
+    const {
+      selectionLevel,
+      selectedProteinSources,
+      selectedFatSources,
+      selectedCarbSources
+    } = this.state;
     return (
       <View style={styles.container}>
-        <DietGoalPlan />
+        {selectionLevel === 1 && (
+          <DietGoalPlan onLevelChange={this.changeSelectionLevel} />
+        )}
+        {selectionLevel === 2 && (
+          <SelectFoodSources
+            onLevelChange={this.changeSelectionLevel}
+            selectedProteins={selectedProteinSources}
+            selectedFats={selectedFatSources}
+            selectedCarbs={selectedCarbSources}
+          />
+        )}
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-})
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
--- src/diet/DietGoalPlan.js
+++ src/diet/DietGoalPlan.js
@@ -1,61 +1,148 @@
-import React, { Component } from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
-import { Input, Button } from 'react-native-elements';
-import { Dropdown } from 'react-native-material-dropdown';
-import NumericInput from 'react-native-numeric-input'
+import React, { Component } from "react";
+import {StyleSheet, Text, View } from "react-native";
+import { Input, Button } from "react-native-elements";
+import { Dropdown } from "react-native-material-dropdown";
+import NumericInput from "react-native-numeric-input";
 
 export default class DietGoalPlan extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      goalDefaultValue: 'Select goal',
-      selectedGoal: '',
-      goals: [{
-        value: 'Fat-loss',
-      },{
-        value: 'Weight-gain',
-      }],
+      goalDefaultValue: "Choose goal !",
+      selectedGoal: "",
+      goals: [
+        {
+          value: "Fat-loss"
+        },
+        {
+          value: "Weight-gain"
+        }
+      ],
+      programs: [
+        { value: "4 Week Program" },
+        { value: "8 Week Program" },
+        { value: "12 Week Program" }
+      ],
+      programDefaultValue: "Choose Program !",
       currentWeight: 75,
-      targetWeight: 75,
-    }
+      targetWeight: 75
+    };
   }
 
   onGoalChange = (value, index) => {
     this.setState({
-      goal: value,
-    })
-  }
+      goal: value
+    });
+  };
 
   render() {
+    const { onLevelChange } = this.props
     return (
-      <View style={{flex:1}}>
+      <View style={{ flex: 1 }}>
+        <View style={{ justifyCentent: 'center', alignItems: 'center', marginVertical: 70 }}>
+          <Text style={{fontSize: 30, fontWeight:'bold', fontStyle:'italic', color:'grey'}}>Getting started...</Text>
+        </View>
         <Dropdown
           label={this.state.goalDefaultValue}
           data={this.state.goals}
-          containerStyle={{width:300, padding:10 }}
-          dropdownOffset={{top:100, left:0}}
+          containerStyle={styles.dropdown}
+          dropdownOffset={{ top: 20, left: 0 }}
         />
-        <View style={{flex:1, flexDirection: 'row'}}>
-          <Text style={{marginVertical:15, marginHorizontal:10}}>Current weight:</Text>
-          <NumericInput 
-              value={this.state.currentWeight} 
-              onChange={value => this.setState({currentWeight: value})}
-              initValue={this.state.currentWeight}
-              totalWidth={80} 
-              totalHeight={40} 
-              iconSize={25}
-              step={1.5}
-              valueType='integer'
-              rounded 
-              textColor='black' 
-              iconStyle={{ color: 'white' }} 
-              rightButtonBackgroundColor='grey' 
-              leftButtonBackgroundColor='grey'/>
+        <Dropdown
+          label={this.state.programDefaultValue}
+          data={this.state.programs}
+          containerStyle={styles.dropdown}
+          dropdownOffset={{ top:20, bottom: 20, left: 0 }}
+        />
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>
+            Current weight:
+          </Text>
+          <NumericInput
+            value={this.state.currentWeight}
+            onChange={value => this.setState({ currentWeight: value })}
+            initValue={this.state.currentWeight}
+            totalWidth={80}
+            totalHeight={40}
+            iconSize={25}
+            step={1.5}
+            valueType="integer"
+            rounded
+            textColor="black"
+            iconStyle={{ color: "white" }}
+            rightButtonBackgroundColor="grey"
+            leftButtonBackgroundColor="grey"
+            containerStyle={styles.numberPicker}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>
+            Target weight:
+          </Text>
+          <NumericInput
+            value={this.state.targetWeight}
+            onChange={value => this.setState({ targetWeight: value })}
+            initValue={this.state.targetWeight}
+            totalWidth={80}
+            totalHeight={40}
+            iconSize={25}
+            step={1.5}
+            valueType="integer"
+            rounded
+            textColor="black"
+            iconStyle={{ color: "white" }}
+            rightButtonBackgroundColor="grey"
+            leftButtonBackgroundColor="grey"
+            containerStyle={styles.numberPicker}
+          />
+        </View>
+        <View style={{flex:1, marginTop: 40}}>
+          <Button
+              title="Next"
+              titleStyle={{ fontWeight: 'bold', fontSize: 18, }}
+            
+              buttonStyle={{
+                borderWidth: 0,
+                borderColor: 'transparent',
+                borderRadius: 20,
+                paddingHorizontal: 15,
+                opacity: 0.7,
+              }}
+              containerStyle={{ marginVertical: 10, marginHorizontal: 20, height: 40, justifyContent: 'center', alignItems: 'flex-end', }}
+              icon={{
+                name: 'arrow-right',
+                type: 'font-awesome',
+                size: 15,
+                color: 'white',
+              }}
+              iconRight
+              iconContainerStyle={{ marginLeft: 5 }}
+              onPress={() => onLevelChange(true)}
+            />
         </View>
       </View>
-    )
+    );
   }
 }
+
+
+const styles = StyleSheet.create({
+  dropdown: {
+    width: 300,
+    padding: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    marginVertical: 15,
+    justifyContent:'space-between',
+    marginHorizontal: 30,
+  },
+  inputLabel: {
+    marginVertical: 15,
+    marginHorizontal: 10,
+    fontSize: 16,
+  },
+  numberPicker: {
+    marginRight:20
+  },
+})
