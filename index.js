72b2e38c8986f62bc7465871a17e9fa103be394d
Dhivya <dhivya@gopalkr-s.redmond.corp.microsoft.com>
2020-04-08 15:44:42 +0530

    Made changes - 2 - iOS required changes - Pods mangement upgrade and 0.58 to 0.62 RN upgrade changes.

--- index.js
+++ index.js
@@ -5,11 +5,12 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-/* Need this for firebase and others to work on android */
+
+/* Need this for firebase and others to work on android 
 global.Symbol = require('core-js/es6/symbol');
 require('core-js/fn/symbol/iterator');
 require('core-js/fn/map');
 require('core-js/fn/set');
-require('core-js/fn/array/find');
+require('core-js/fn/array/find');*/
 
 AppRegistry.registerComponent(appName, () => App);
